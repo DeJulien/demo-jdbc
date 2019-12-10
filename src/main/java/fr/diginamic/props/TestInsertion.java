dc541b491d0fb33890239ca23d1321ef75cf62f6
Julien Dewost <dewostjulien@gmail.com>
2019-12-10 15:17:16 +0100

    JDBC – TP N°03 – Operations CRUD exo1

--- src/main/java/fr/diginamic/props/TestInsertion.java
+++ src/main/java/fr/diginamic/props/TestInsertion.java
@@ -1,10 +1,49 @@
 package fr.diginamic.props;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
 public class TestInsertion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
+		String driverName = monFichierConf.getString("database.driver");
+		String url=monFichierConf.getString("database.url");
+		String user=monFichierConf.getString("database.user");
+		String password="";
+		
+		
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println(connection);
+			
+			Statement monStatement = connection.createStatement();
+			int nb = monStatement.executeUpdate("INSERT INTO FOURNISSEUR (ID,NOM) VALUES (4,'La Maison de la Peinture')");
+			connection.commit();
+			
+			monStatement.close();
+			connection.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
