6b9661cd1884bf9fed00310636e57eb75b558d7e
Julien Dewost <dewostjulien@gmail.com>
2019-12-10 13:41:53 +0100

    JDBC – TP N°01 - Connexion

--- src/main/java/fr/diginamic/props/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/props/TestConnexionJdbc.java
@@ -5,21 +5,31 @@ package fr.diginamic.props;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) {
-
+		
+		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
+		String driverName = monFichierConf.getString("database.driver");
+		String url=monFichierConf.getString("database.url");
+		String user=monFichierConf.getString("database.user");
+		String password="";
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+			Class.forName(driverName);
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/compta", "root", "");
+			Connection connection = DriverManager.getConnection(url, user, password);
 			System.out.println("connection open");
+			
+
+			
 			connection.close();
 			System.out.println("connection close");
 		} catch (SQLException e) {
